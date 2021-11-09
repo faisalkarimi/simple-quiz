2f2124064d62474bb60df77ffcb3ad6f1c91fa47
ahfaisalkarimi <ahfaisalkarimi@gmail.com>
2021-11-09 12:04:14 -0800

    Fixed styling

--- src/components/QuestionContainer.js
+++ src/components/QuestionContainer.js
@@ -20,37 +20,39 @@ const QuestionContainer = () => {
   return (
     <div className="quiz-container" id="quiz">
       {state.showScore ? (
-        <div className="quiz-header">
-          <h2>Your Score is {state.score}</h2>
+        <>
+          <div className="quiz-header">
+            <h2>Your Score is {state.score}</h2>
+          </div>
           <button onClick={() => dispatch({ type: "RESET_SCORE" })}>
             Restart Quiz
           </button>
-        </div>
+        </>
+      ) : currentQuestion < quizData.length ? (
+        <>
+          <div className="quiz-header">
+            <Question
+              question={quizData[currentQuestion].question}
+              answer={quizData[currentQuestion].correct}
+              option_a={quizData[currentQuestion].a}
+              option_b={quizData[currentQuestion].b}
+              option_c={quizData[currentQuestion].c}
+              option_d={quizData[currentQuestion].d}
+              id={quizData[currentQuestion].correct}
+            />
+          </div>
+          <button id="submit" onClick={handleNext}>
+            Submit
+          </button>
+        </>
       ) : (
         <>
-          {currentQuestion < quizData.length ? (
-            <div className="quiz-header">
-              <Question
-                question={quizData[currentQuestion].question}
-                answer={quizData[currentQuestion].correct}
-                option_a={quizData[currentQuestion].a}
-                option_b={quizData[currentQuestion].b}
-                option_c={quizData[currentQuestion].c}
-                option_d={quizData[currentQuestion].d}
-                id={quizData[currentQuestion].correct}
-              />
-              <button id="submit" onClick={handleNext}>
-                Submit
-              </button>
-            </div>
-          ) : (
-            <div className="quiz-header">
-              <h2>End of Quiz</h2>
-              <button id="submit" onClick={handleNext}>
-                Show Score
-              </button>
-            </div>
-          )}
+          <div className="quiz-header">
+            <h2>End of Quiz</h2>
+          </div>
+          <button id="submit" onClick={handleNext}>
+            Show Score
+          </button>
         </>
       )}
     </div>
